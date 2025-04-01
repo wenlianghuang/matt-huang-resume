eeb6aced8c1a1d6c9e27a7b6b4ce400cc1218cb8
Matt Huang <wenliangmatt@gmail.com>
2025-04-01 19:04:42 +0800

    modified file

--- app/bkendex/page.tsx
+++ app/bkendex/page.tsx
@@ -1,15 +1,31 @@
 'use client';
 import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function Bkendex() {
   const [data, setData] = useState("");
-
+  const [time,setTime] = useState("");
   useEffect(() => {
     fetch("https://matt-huang-resume-backend.vercel.app/api/data")
       .then((res) => res.json())
-      .then((json) => setData(json.message))
+      .then((json) => {setData(json.message),setTime(json.log)})
       .catch((err) => console.error("API 錯誤:", err));
   }, []);
 
-  return <h1>{data || "Loading..."}</h1>;
+  return (
+    <>
+      <div>
+        <h1>{data || "Loading..."}</h1>
+      </div>
+      <div>
+        <h2>{time || ""}</h2>
+      </div>
+    </>
+  );
+
+  /*
+  return
+  (
+    <h1>{data || "Loading..."}</h1>
+  ) 
+    */
 }
